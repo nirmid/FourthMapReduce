f9e43a8cc5c526c665a565d3e3dfff825e60a65e
nir <nirmid@post.bgu.ac.il>
2022-12-26 19:39:59 +0200

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -70,10 +70,10 @@ public class Main {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         //TODO
-        //FileInputFormat.addInputPath(job, new Path(args[1]));
-        //FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        //FileInputFormat.addInputPath(job, new Path(args[0]));
+        //FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
